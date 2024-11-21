4677b1eae05e5babc67e20994704bfea7f06ba7c
Siavash Ghaffari <siavashghaffari@SIAVASH-MacCA.local>
2024-11-20 22:30:20 -0500

    minor revision in the readme file

--- Concat/crop-seq-pipeline/src/Templates/tools/rapids_modified.py
+++ Concat/crop-seq-pipeline/src/Templates/tools/rapids_modified.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2020, NVIDIA CORPORATION.
 # Modified by Siavash Ghaffari to be consistent with the crop-seq-pipeline
-# Modified by Rahul Mohan from GENENTECH in 2023.
+
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
